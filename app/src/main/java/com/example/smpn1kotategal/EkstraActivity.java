332072a7f7037b8e1f7574e16a66d78b2f675dda
Dwiky16 <dwikydarmawan2004@gmail.com>
2022-03-09 12:37:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/smpn1kotategal/EkstraActivity.java
@@ -0,0 +1,45 @@
+package com.example.smpn1kotategal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class EkstraActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.poster1,
+            R.drawable.poster2,
+            R.drawable.poster3,
+            R.drawable.poster1};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekstra);
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+
+    public void pramuka(View view) {
+    }
+
+    public void pmr(View view) {
+    }
+
+    public void futsal(View view) {
+    }
+
+    public void volly(View view) {
+    }
+}
